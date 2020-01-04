843b917a59e5f78dce8b64f017e2c6407eaf7d49
ChayanRin <k852012@naver.com>
2020-01-04 17:35:50 +0900

    Create flexbox contents

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 
+var call = document.querySelector(".call");
+
+function callClick(){
+    document.location.href='tel:02-833-1751'
+}
 
 function init(){
-
+    call.addEventListener('click', callClick)
 }
 init()
