a5c8360853f372d763887ddb850af65a2538cd80
ChayanRin <k852012@naver.com>
2020-01-05 17:25:52 +0900

    Update articles content

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 
 var call = document.querySelector(".call");
 
+
+
 function callClick(){
     document.location.href='tel:02-833-1751'
 }
 
+
+
+
+
 function init(){
     call.addEventListener('click', callClick)
+
 }
 init()
